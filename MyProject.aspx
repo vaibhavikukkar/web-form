9f182d732c6571298f9172a7d95572d39e7a56cf
vaibhavikukkar <149795748+vaibhavikukkar@users.noreply.github.com>
2024-01-17 21:17:55 +0530

    Add files via upload

---
+++ MyProject.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MyProject.aspx.cs" Inherits="WebApplication3.MyProject" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+<style>
+td{
+    color:#2f3c7e;
+}
+</style>
+</head>
+<body style="background-color:#fbeaeb; height: 292px;">
+    <h1 style="color:#2f3c7e;text-align:center;">Student Data</h1>
+    <form id="form1" runat="server">
+        <div>
+            <table align="center">
+                <tr>
+                    <td>ID</td>
+                    <td>
+                        <asp:TextBox ID="txtID" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Name</td>
+                    <td>
+                        <asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Phone</td>
+                    <td>
+                        <asp:TextBox ID="txtPhone" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Address</td>
+                    <td>
+                        <asp:TextBox ID="txtAdd" runat="server"></asp:TextBox></td>
+                </tr>
+                <br />
+                <tr>
+                    <td>
+                        <asp:Button style="background-color:#2f3c7e;color:#fbeaeb;box-shadow:2px 2px 5px #2f3c7e;" ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" /></td>
+                    <td>
+                        <asp:Button style="background-color:#2f3c7e;color:#fbeaeb;box-shadow:2px 2px 5px #2f3c7e;" ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />    
+                        <asp:Button style="background-color:#2f3c7e;color:#fbeaeb;box-shadow:2px 2px 5px #2f3c7e;" ID="btnUpdate" runat="server" Text="Update" OnClick="btnupdate_Click" /></td>
+                    <td>
+    <asp:Button style="background-color:#2f3c7e;color:#fbeaeb;box-shadow:2px 2px 5px #2f3c7e;" ID="btnFetchAll" runat="server" Text="Fetch All" OnClick="btnfetchall_Click" Width="94px" Height="33px" /></td>
+                                    <td>
+<asp:Button style="background-color:#2f3c7e;color:#fbeaeb;box-shadow:2px 2px 5px #2f3c7e;" ID="btnGo" runat="server" Text="Go" OnClick="btngo_Click" Width="94px" Height="33px" /></td>
+                </tr>
+            </table>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" BorderStyle="Ridge" BorderWidth="3px" HorizontalAlign="Center" style="margin-top: 53px" Width="1197px">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ MyProject.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace WebApplication3
+{
+    public partial class MyProject : System.Web.UI.Page
+    {
+        SqlDataAdapter da;
+        DataTable dt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-JCM7G8I\\SQLEXPRESS;Initial Catalog=Vaibhu;Integrated Security=True;User ID=DESKTOP-JCM7G8I");
+                SqlCommand cmd = new SqlCommand(@"INSERT INTO [Vaibhu].[dbo].[Student]" +
+                "([ID],[Name],[Phone],[Address]) VALUES ('" + txtID.Text + "','" + txtName.Text + "','" + txtPhone.Text + "','" + txtAdd.Text + "')", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Response.Write("data inserted successfully");
+                con.Close();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-JCM7G8I\\SQLEXPRESS;Initial Catalog=Vaibhu;Integrated Security=True;User ID=DESKTOP-JCM7G8I");
+                SqlCommand cmd = new SqlCommand(@"DELETE FROM[Vaibhu].[dbo].[Student]
+                WHERE [ID]='" + txtID.Text + "'", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Response.Write("data deleted successfully");
+                con.Close();
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-JCM7G8I\\SQLEXPRESS;Initial Catalog=Vaibhu;Integrated Security=True;User ID=DESKTOP-JCM7G8I");
+                SqlCommand cmd = new SqlCommand("UPDATE [Vaibhu].[dbo].[Student]   SET [ID] = '" + txtID.Text + "',[Name] = '" + txtName.Text + "',[Phone] = '" + txtPhone.Text + "',[Address] = '" + txtAdd.Text + "' WHERE [ID]='" + txtID.Text + "'", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Response.Write("data updated successfully");
+                con.Close();
+            
+        }
+
+        protected void btnfetchall_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-JCM7G8I\\SQLEXPRESS;Initial Catalog=Vaibhu;Integrated Security=True;User ID=DESKTOP-JCM7G8I");
+            da = new SqlDataAdapter("select * from Student;", con);
+            dt = new DataTable();
+            da.Fill(dt);
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void btngo_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-JCM7G8I\\SQLEXPRESS;Initial Catalog=Vaibhu;Integrated Security=True;User ID=DESKTOP-JCM7G8I");
+            int ID = Convert.ToInt32(txtID.Text);
+
+            da = new SqlDataAdapter("select * from Student WHERE ID="+ID+"", con);
+            dt = new DataTable();   
+            da.Fill(dt);
+
+            txtID.Text = dt.Rows[0][0].ToString();
+            txtName.Text = dt.Rows[0][1].ToString();
+            txtPhone.Text = dt.Rows[0][2].ToString();
+            txtAdd.Text = dt.Rows[0][3].ToString();
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
